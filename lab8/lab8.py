d488d0ced9b07d0130b032bc6f6c051ac472984f
Muhammad Hinan Ali <g00409830@atu.ie>
2023-11-20 16:05:44 +0000

    imggray variable added and displays

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -4,4 +4,12 @@ from matplotlib import pyplot as plt
 
 print("Hello World") #TO SEE WORKING OR NOT
 
-img = cv2.imread('ATU.jpg',)
+img = cv2.imread('ATU.jpg',) #img Variable to image read the jpg/photo
+cv2.imshow('Coloured', img) #Shows/Displays img variable
+cv2.waitKey(0)  
+
+# Convert to Grayscale
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Grey Scale', imgGray) #Shows/Displays img variable
+cv2.waitKey(0) 
+
