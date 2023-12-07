2b105fdb777b8416591d7d99659062e62f6daea9
Muhammad Hinan Ali <g00409830@atu.ie>
2023-12-07 19:12:35 +0000

    further kernel code

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -20,3 +20,17 @@ plt.show()
 
 # Apply GaussianBlur with different kernel sizes
 kernel_sizes = [3, 5, 9, 13]
+
+plt.figure(figsize=(10, 10))
+plt.subplot(2, 2, 1), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title('Original Grayscale'), plt.xticks([]), plt.yticks([])
+
+for i, kernel_size in enumerate(kernel_sizes):
+    if i < 3:  # Limit the number of subplots to 3
+
+        imgOut = cv2.GaussianBlur(imgGray, (kernel_size, kernel_size), 0)
+        
+        # Plotting images with Matplotlib
+        plt.subplot(2, 2, i + 2), plt.imshow(imgOut, cmap='gray')
+        plt.title(f'Blurred (Kernel {kernel_size})'), plt.xticks([]), plt.yticks([])
+plt.show()
