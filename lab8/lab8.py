bd85033042e55f9f7d6cf5959dd935308911540d
Muhammad Hinan Ali <g00409830@atu.ie>
2023-12-07 20:48:22 +0000

    mathplot for the end part

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -9,11 +9,15 @@ img = cv2.imread('ATU.jpg')
 # Convert the image to grayscale
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#nrow and ncols
+nrow = 2
+ncol = 1
+
 # Plotting images with Matplotlib
-plt.subplot(2, 1, 1), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.subplot(nrow, ncol, 1), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(2, 1, 2), plt.imshow(imgGray, cmap='gray')
+plt.subplot(nrow, ncol, 2), plt.imshow(imgGray, cmap='gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
 plt.show()
@@ -40,19 +44,19 @@ sobelHorizontal = cv2.Sobel(imgGray, cv2.CV_64F, 1, 0, ksize=5)
 sobelVertical = cv2.Sobel(imgGray, cv2.CV_64F, 0, 1, ksize=5)
 
 # Plot Sobel output images
-plt.subplot(1, 2, 1), plt.imshow(sobelHorizontal, cmap='gray')
-plt.title('Sobel Horizontal'), plt.xticks([]), plt.yticks([])
-plt.subplot(1, 2, 2), plt.imshow(sobelVertical, cmap='gray')
-plt.title('Sobel Vertical'), plt.xticks([]), plt.yticks([])
-plt.show()
+#plt.subplot(1, 2, 1), plt.imshow(sobelHorizontal, cmap='gray')
+#plt.title('Sobel Horizontal'), plt.xticks([]), plt.yticks([])
+#plt.subplot(1, 2, 2), plt.imshow(sobelVertical, cmap='gray')
+#plt.title('Sobel Vertical'), plt.xticks([]), plt.yticks([])
+#plt.show()
 
 # Combine horizontal and vertical edges
 sobelCombined = cv2.addWeighted(sobelHorizontal, 0.5, sobelVertical, 0.5, 0)
 
 # Plot combined Sobel output
-plt.imshow(sobelCombined, cmap='gray')
-plt.title('Combined Sobel Edges'), plt.xticks([]), plt.yticks([])
-plt.show()
+#plt.imshow(sobelCombined, cmap='gray')
+#plt.title('Combined Sobel Edges'), plt.xticks([]), plt.yticks([])
+#plt.show()
 
 # Canny edge detection
 cannyThreshold = 100
@@ -60,6 +64,24 @@ cannyParam2 = 200
 canny = cv2.Canny(imgGray, cannyThreshold, cannyParam2)
 
 # Plot Canny edge detection result
-plt.imshow(canny, cmap='gray')
+#plt.imshow(canny, cmap='gray')
+#plt.title('Canny Edge Detection'), plt.xticks([]), plt.yticks([])
+#plt.show()
+
+#nrow and ncols
+nrow = 2
+ncol = 3
+
+plt.subplot(nrow, ncol, 1), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrow, ncol, 2), plt.imshow(imgGray, cmap='gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrow, ncol, 3), plt.imshow(sobelHorizontal, cmap='gray')
+plt.title('Sobel Horizontal'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrow, ncol, 4), plt.imshow(sobelVertical, cmap='gray')
+plt.title('Sobel Vertical'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrow, ncol, 5),plt.imshow(sobelCombined, cmap='gray')
+plt.title('Combined Sobel Edges'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrow, ncol, 6),plt.imshow(canny, cmap='gray')
 plt.title('Canny Edge Detection'), plt.xticks([]), plt.yticks([])
 plt.show()
