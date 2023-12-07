5bfffd893843a8712a551e3450678179e0b2a3d5
Muhammad Hinan Ali <g00409830@atu.ie>
2023-12-07 18:40:07 +0000

    code updated

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -1,29 +1,19 @@
+# Import OpenCV, NumPy, and Matplotlib
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-print("Hello World") #TO SEE WORKING OR NOT
+# Load the image
+img = cv2.imread('ATU.jpg')
 
-img = cv2.imread('ATU.jpg',) #img Variable to image read the jpg/photo #https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python
-cv2.imshow('Original', img) #Shows/Displays img variable
-cv2.waitKey(0)  
-
-# Convert to Grayscale
+# Convert the image to grayscale
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('GrayScale', imgGray) #Shows/Displays imgGray variable
-cv2.waitKey(0) 
 
-#nrows and ncols variable
-nrows = 2
-ncols = 1
-
-#Convert the channels
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-#displays photos together using subplot/mathplot 
-plt.subplot(nrows, ncols,1),plt.imshow(img , cmap = 'gray')
+# Plotting images with Matplotlib
+plt.subplot(2, 1, 1), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(nrows, ncols,2),plt.imshow(imgGray, cmap = 'gray')
-plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
-plt.show()
 
+plt.subplot(2, 1, 2), plt.imshow(imgGray, cmap='gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+
+plt.show()
